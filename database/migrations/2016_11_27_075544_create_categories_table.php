083fb06e5e4f1e67ba1eff9f692dc3673b0a3082
seekerliu <seekerliu@vip.qq.com>
2016-11-29 01:54:49 +0800

    fix Add Category

--- database/migrations/2016_11_27_075544_create_categories_table.php
+++ database/migrations/2016_11_27_075544_create_categories_table.php
@@ -17,7 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->integer('order')->unsigned()->default(0);
-            $table->boolean('stick');
+            $table->boolean('stick')->default(0);
             $table->timestamps();
         });
     }
